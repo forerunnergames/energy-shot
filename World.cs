7b9e02d761e6760667f5eea303b91ecb133c1a3a
Aaron Mahan <aaron@forerunnergames.com>
2025-03-02 18:02:02 -0800

    Add energy weapon spin & quit dialog.
    Other:
    
    - Fix GdUnit4 failing in GitHub Actions by upgrading to the latest
      version.

--- World.cs
+++ World.cs
@@ -7,16 +7,21 @@ public partial class World : Node3D
   private ENetMultiplayerPeer _peer = new();
   private PanelContainer _mainMenu = null!;
   private Control _hud = null!;
+  private ConfirmationDialog2 _quitDialog = null!;
   private PackedScene _playerScene = null!;
   private ProgressBar _healthBar = null!;
+  private Label _scoreLabel = null!;
   private HostGameDialog _hostGameDialog = null!;
   private JoinGameDialog _joinGameDialog = null!;
   private Button _hostButton = null!;
   private Button _joinButton = null!;
   private Button _quitButton = null!;
+  private Player? _player;
+  private int _score;
+  private void QuitGame() => GetTree().Quit();
   private void OnJoinButtonPressed() => _joinGameDialog.Show (_peer, ServerPort);
   private void OnHostButtonPressed() => _hostGameDialog.Show (_peer, ServerPort);
-  private void OnQuitButtonPressed() => GetTree().Quit();
+  private void OnQuitButtonPressed() => QuitGame();
   private void RemovePlayer (long peerId) => GetNodeOrNull <Player> ($"{peerId}")?.QueueFree();
 
   public override void _Ready()
@@ -24,12 +29,17 @@ public partial class World : Node3D
     _playerScene = ResourceLoader.Load <PackedScene> ("res://Player.tscn");
     _mainMenu = GetNode <PanelContainer> ("UI/MainMenu");
     _hud = GetNode <Control> ("UI/HUD");
+    _quitDialog = GetNode <ConfirmationDialog2> ("UI/QuitDialog");
     _healthBar = GetNode <ProgressBar> ("UI/HUD/HealthBar");
+    _scoreLabel = GetNode <Label> ("UI/HUD/Score");
     _hostGameDialog = GetNode <HostGameDialog> ("UI/HostGameDialog");
     _joinGameDialog = GetNode <JoinGameDialog> ("UI/JoinGameDialog");
     _hostButton = GetNode <Button> ("UI/MainMenu/MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/HostButton");
     _joinButton = GetNode <Button> ("UI/MainMenu/MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/JoinButton");
     _quitButton = GetNode <Button> ("UI/MainMenu/MarginContainer/VBoxContainer/MarginContainer/VBoxContainer/Quit");
+    _quitDialog.Confirmed += QuitGame;
+    _quitDialog.Canceled += CancelQuit;
+    _quitDialog.Closed += CancelQuit;
     _hostButton.Pressed += OnHostButtonPressed;
     _joinButton.Pressed += OnJoinButtonPressed;
     _quitButton.Pressed += OnQuitButtonPressed;
@@ -40,7 +50,27 @@ public partial class World : Node3D
   public override void _UnhandledInput (InputEvent @event)
   {
     if (!Input.IsActionJustPressed ("quit")) return;
-    GetTree().Quit();
+    ToggleQuitDialog();
+  }
+
+  private void ToggleQuitDialog()
+  {
+    if (_quitDialog.Visible)
+    {
+      _quitDialog.Hide();
+      CancelQuit();
+      return;
+    }
+
+    _quitDialog.Show();
+    Input.MouseMode = Input.MouseModeEnum.Visible;
+    _player?.SetInputEnabled (isEnabled: false);
+  }
+
+  private void CancelQuit()
+  {
+    Input.MouseMode = Input.MouseModeEnum.Captured;
+    _player?.SetInputEnabled (isEnabled: true);
   }
 
   private void OnHostGameSuccess()
@@ -91,6 +121,13 @@ public partial class World : Node3D
     player.Name = $"{peerId}";
     AddChild (player);
     if (!player.IsMultiplayerAuthority()) return;
-    player.HealthChanged += value => _healthBar.Value = value;
+    _player = player;
+    _player.HealthChanged += value => _healthBar.Value = value;
+
+    _player.Scored += () =>
+    {
+      _scoreLabel.Text = $"{++_score}";
+      GD.Print ($"Score: {_score}");
+    };
   }
 }
