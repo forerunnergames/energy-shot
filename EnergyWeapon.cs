82b58ee31cccc35bd05e28445ad2936533a2e4ea
Aaron Mahan <aaron@forerunnergames.com>
2025-03-11 16:49:56 -0700

    Weapon Firing v2
    - Charge weapon while holding fire button, increasing energy.
    - Discharge weapon when releasing fire button, releasing an energy shot.
    - Add pink glow as weapon charges & vice versa.
    - Add shooter throwback proportional to the weapon's discharged energy.
    - Synchronize the weapon color changes from other players.
    - Regression: Synchronize shooting sounds from other players.
    - Increase starting health to 100 to allow for varying energy hits.
    
    - Decrease player health proportional to the hit's energy, rewarding
      players with a one-shot KO when a fully-charged shot connects.

--- EnergyWeapon.cs
+++ EnergyWeapon.cs
@@ -2,47 +2,86 @@ using Godot;
 
 public partial class EnergyWeapon : Node3D
 {
+  // @formatter:off
+
+  [Export]
+  public Color WeaponColor
+  {
+    get => _weaponColor;
+    set
+    {
+      _weaponColor = value;
+      _muzzleMaterial.AlbedoColor = _weaponColor;
+    }
+  }
+
   [Export] public float MinRotationSpeed = 1.0f;
   [Export] public float MaxRotationSpeed = 15.0f;
-  [Signal] public delegate void ShotEventHandler();
+  [Signal] public delegate void ShotFiredEventHandler (float energy);
   public float CurrentRotationSpeed { get; private set; }
-  public bool IsShooting { get; private set; }
+  public bool IsSpinningUp { get; private set; }
   private AudioStreamPlayer3D _shootingSound = null!;
-  private Node3D _muzzle = null!;
+  private MeshInstance3D _muzzleMeshInstance = null!;
   private Node3D _pivot = null!;
+  private StandardMaterial3D _muzzleMaterial = null!;
+  private Color _normalColor;
+  private Color _chargedColor;
+  private Color _weaponColor;
   private Tween? _tween;
   public override void _PhysicsProcess (double delta) => _pivot.Rotate (Vector3.Right, CurrentRotationSpeed * (float)delta);
-  public void Shoot() => SpinUp();
+  public void PlayShootingSound() => _shootingSound.Play();
+  public void Charge() => SpinUp();
+  private float CalculateEnergy() => CurrentRotationSpeed / MaxRotationSpeed;
+  // @formatter:on
 
   public override void _Ready()
   {
-    _muzzle = GetNode <Node3D> ("Pivot/Muzzle");
     _pivot = GetNode <Node3D> ("Pivot");
     _shootingSound = GetNode <AudioStreamPlayer3D> ("ShootingSound");
+    _muzzleMeshInstance = GetNode <Node3D> ("Pivot/Muzzle").GetNode <MeshInstance3D> ("Cube_001");
+    _muzzleMaterial = CreateCopy ((_muzzleMeshInstance.Mesh.SurfaceGetMaterial (0) as StandardMaterial3D)!);
+    _muzzleMeshInstance.MaterialOverride = _muzzleMaterial;
+    _normalColor = _muzzleMaterial.AlbedoColor;
+    _chargedColor = new Color (3.0f, 0.0f, _muzzleMaterial.AlbedoColor.B, _muzzleMaterial.AlbedoColor.A);
+    WeaponColor = _normalColor;
     CurrentRotationSpeed = MinRotationSpeed;
+  }
+
+  public void Discharge()
+  {
+    EmitSignal (SignalName.ShotFired, CalculateEnergy());
+    SpinDown();
   }
 
   private void SpinUp()
   {
-    IsShooting = true;
+    if (IsSpinningUp) return;
+    IsSpinningUp = true;
     _tween?.Kill();
-    _tween = CreateTween();
+    _tween = CreateTween().SetParallel();
     _tween.TweenProperty (this, "CurrentRotationSpeed", MaxRotationSpeed, 2.0f).SetTrans (Tween.TransitionType.Quad).SetEase (Tween.EaseType.Out);
-    _tween.Finished += FireShot;
-  }
-
-  private void FireShot()
-  {
-    _shootingSound.Play();
-    EmitSignal (SignalName.Shot);
-    SpinDown();
+    _tween.TweenProperty (this, "WeaponColor", _chargedColor, 2.0f).SetTrans (Tween.TransitionType.Quad).SetEase (Tween.EaseType.In);
   }
 
   private void SpinDown()
   {
+    IsSpinningUp = false;
     _tween?.Kill();
-    _tween = CreateTween();
+    _tween = CreateTween().SetParallel();
     _tween.TweenProperty (this, "CurrentRotationSpeed", MinRotationSpeed, 2.0f).SetTrans (Tween.TransitionType.Quad).SetEase (Tween.EaseType.Out);
-    _tween.Finished += () => IsShooting = false;
+    _tween.TweenProperty (this, "WeaponColor", _normalColor, 2.0f).SetTrans (Tween.TransitionType.Quad).SetEase (Tween.EaseType.Out);
+  }
+
+  private StandardMaterial3D CreateCopy (StandardMaterial3D material)
+  {
+    var copy = new StandardMaterial3D();
+    copy.AlbedoColor = material.AlbedoColor;
+    copy.AlbedoTexture = material.AlbedoTexture;
+    copy.Metallic = material.Metallic;
+    copy.MetallicSpecular = material.MetallicSpecular;
+    copy.Roughness = material.Roughness;
+    copy.EmissionEnabled = material.EmissionEnabled;
+    copy.Emission = material.Emission;
+    return copy;
   }
 }
