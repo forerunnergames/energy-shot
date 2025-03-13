ff514672f9b529ea6f81087c3654ff9bd92651b1
Aaron Mahan <aaron@forerunnergames.com>
2025-03-12 18:05:26 -0700

    Add names, health, & messages.
    - Show player name billboards.
    - Show player health bar billboards.
    - Show messages when players join, leave, & score on each other.
    - Add lower world bound to prevent falling forever.
    - Show reason on main menu screen when disconnected from server.

--- Tools.cs
+++ Tools.cs
@@ -1,21 +1,29 @@
 using System.Text.RegularExpressions;
 using Godot;
 
-namespace energyshot;
+namespace com.forerunnergames.energyshot;
 
 public static partial class Tools
 {
   // @formatter:off
+  public static bool IsValidServerAddress (string address) => IsValidIPv4 (address) || IsValidIPv6 (address) || IsValidHostname (address);
+  public static bool IsValidPlayerName (string name) => ValidPlayerNameRegex().IsMatch (name);
   [GeneratedRegex (@"^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$")]
   private static partial Regex ValidIPv4Regex();
   [GeneratedRegex (@"^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}(([0-9]{1,3}\.){3,3}[0-9]{1,3})|([0-9a-fA-F]{1,4}:){1,4}:(([0-9]{1,3}\.){3,3}[0-9]{1,3}))$")]
   private static partial Regex ValidIPv6Regex();
-  [GeneratedRegex (@"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,})+$")] private static partial Regex ValidHostnameRegex();
-  public static bool IsValidServerAddress (string address) => IsValidIPv4 (address) || IsValidIPv6 (address) || IsValidHostname (address);
+  [GeneratedRegex (@"^(?!-)[A-Za-z0-9-]{1,63}(?<!-)(\.[A-Za-z]{2,})+$")]
+  private static partial Regex ValidHostnameRegex();
+  [GeneratedRegex ("^[A-Za-z0-9]{1,16}$")]
+  private static partial Regex ValidPlayerNameRegex();
+  private static bool IsValidIPv4 (string address) => ValidIPv4Regex().IsMatch (address);
+  private static bool IsValidIPv6 (string address) => ValidIPv6Regex().IsMatch (address);
+  private static bool IsValidHostname (string address) => ValidHostnameRegex().IsMatch (address);
   // @formatter:on
 
   public static (bool success, string address, string error) FindServerAddress (int port)
   {
+    return (true, "127.0.0.1", string.Empty); // TODO Remove
     var uPnp = new Upnp();
     var discoverResult = (Upnp.UpnpResult)uPnp.Discover();
 
@@ -51,23 +59,5 @@ public static partial class Tools
     var address = uPnp.QueryExternalAddress();
     GD.Print ($"UPNP setup successfully, server address [{address}]");
     return (true, address, string.Empty);
-  }
-
-  private static bool IsValidIPv4 (string address)
-  {
-    var regex = ValidIPv4Regex();
-    return regex.IsMatch (address);
-  }
-
-  private static bool IsValidIPv6 (string address)
-  {
-    var regex = ValidIPv6Regex();
-    return regex.IsMatch (address);
-  }
-
-  private static bool IsValidHostname (string address)
-  {
-    var regex = ValidHostnameRegex();
-    return regex.IsMatch (address);
   }
 }
